56040bdbdc7be99cf24e01c537d3a183ebd0531f
ianhuang <ian_huang32@163.com>
2022-01-06 00:15:21 +0000

    add runtime mode

--- LandscapeGenWnd.py
+++ LandscapeGenWnd.py
@@ -9,6 +9,8 @@ class LandscapeGenWnd:
     def __init__(self):
         self.m_landscapeType = 'Grass'
         self.m_historyList = []
+        self.m_isRumtimeMode = False
+        self.m_isDirty = True
 
     def createGenWnd(self, pWindowTitle):
         windowID = 'LandscapeGenerator'
@@ -22,30 +24,42 @@ class LandscapeGenWnd:
 
     def genWndGUI(self, wndID):
         with LayoutManager(cmds.columnLayout(columnAttach=('left', 5), rowSpacing=10, cw=300)):
+            cmds.checkBox('RuntimeMode', label='Runtime Mode (Not Recommanded)', ann='In this mode, generation will update everytime the values of following parameters are changed, unless \"Apply\" button clicked. \nIn order to gaurantee the calculation speed, the MAX VALUE of density will be limited.', value=False, changeCommand=self.runtimeModeOnChg)
             typeMenu = cmds.optionMenu('LandscapeType', label='Type ')
             cmds.menuItem('Grass', label='Grass', parent=typeMenu)
             cmds.menuItem('Bush', label='Bush', parent=typeMenu)
             cmds.menuItem('Tree', label='Tree', parent=typeMenu)
             cmds.menuItem('Rock', label='Rock', parent=typeMenu)
-            cmds.intSliderGrp('Density', field=True, label='Density ')
+            cmds.intSliderGrp('Density', field=True, label='Density ', changeCommand=self.updateGeneration)
             with LayoutManager(cmds.columnLayout(columnAttach=('left', 5), rowSpacing=10, cw=300)) as ctrlTab:
-                cmds.floatSliderGrp('MinScale', field=True, label='Min Scale ', minValue=0.0, value=1.0)
-                cmds.floatSliderGrp('MaxScale', field=True, label='Max Scale ', minValue=0.0, value=1.0)
+                cmds.floatSliderGrp('MinScale', field=True, label='Min Scale ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration)
+                cmds.floatSliderGrp('MaxScale', field=True, label='Max Scale ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration)
                 cmds.optionMenu(typeMenu, e=True, changeCommand=partial(self.menuItemOnChg, ctrlTab=ctrlTab))
             
             cmds.separator()
-            dirBtnGrp = cmds.radioButtonGrp('GrowingDirection', label='Growing Direction ', nrb=3, labelArray3=['Up', 'Surface Normal', 'Customized'], sl=2, changeCommand3=self.customizedBtnOnChg)
-            cmds.floatFieldGrp('CustomizedDir', numberOfFields=3, label='xyz', v1=0.0, v2=1.0, v3=0.0, vis=False)
-            cmds.checkBox('NaturalGrowing', label='Natural Growing',  parent=dirBtnGrp)
+            dirBtnGrp = cmds.radioButtonGrp('GrowingDirection', label='Growing Direction ', nrb=3, labelArray3=['Up', 'Surface Normal', 'Customized'], sl=2, changeCommand3=self.customizedBtnOnChg, changeCommand=self.updateGeneration)
+            cmds.floatFieldGrp('CustomizedDir', numberOfFields=3, label='xyz', v1=0.0, v2=1.0, v3=0.0, vis=False, changeCommand=self.updateGeneration)
+            cmds.checkBox('NaturalGrowing', label='Natural Growing', changeCommand=self.updateGeneration,  parent=dirBtnGrp)
 
             cmds.separator()
             cmds.separator()
             cmds.separator()
-            cmds.button('Undo', label='Undo', command=self.undoCallback)
+            with LayoutManager(cmds.rowLayout(columnAttach=(1, 'left', 10))):
+                cmds.button('Undo', label='Undo', command=self.undoCallback, width=435)
+            cmds.separator()
         with LayoutManager(cmds.rowLayout(nc=3, adj=True, columnWidth=[(1,150), (2,150), (3,150)], columnAttach=[(1, 'both', 10), (2, 'both', 10), (3, 'both', 10)])):
             cmds.button('Accept', label='Accept', command=partial(self.acceptCallback, wndID))
             cmds.button('Apply', label='Apply', command=self.applyCallback)
             cmds.button('Cancel', label='Cancel', command=partial(self.cancelCallback, wndID))
+
+    def runtimeModeOnChg(self, res):
+        self.m_isRumtimeMode = res
+        if res:
+            self.m_isDirty = False
+            cmds.intSliderGrp('Density', e=True, maxValue=60)
+        else:
+            self.m_isDirty = True
+            cmds.intSliderGrp('Density', e=True, maxValue=200)
 
 
     def menuItemOnChg(self, opt, ctrlTab):
@@ -57,14 +71,14 @@ class LandscapeGenWnd:
 
         if opt == 'Rock':
             cmds.text('GeneralSize', label='General Size: ', parent=ctrlTab)
-            cmds.floatSliderGrp('MinScale', field=True, label='Min Size ', minValue=0.0, value=1.0, parent=ctrlTab)
-            cmds.floatSliderGrp('MaxScale', field=True, label='Max Size ', minValue=0.0, value=1.0, parent=ctrlTab)
+            cmds.floatSliderGrp('MinScale', field=True, label='Min Size ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration, parent=ctrlTab)
+            cmds.floatSliderGrp('MaxScale', field=True, label='Max Size ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration, parent=ctrlTab)
             cmds.text('SingleRock', label='Shape Turbulence of Single Rock: ', parent=ctrlTab)
-            cmds.floatSliderGrp('SizeTurb', field=True, label='Size Turbulence', minValue=0.0, value=0.0, parent=ctrlTab)
-            cmds.floatSliderGrp('ShapeTurb', field=True, label='Shape Turbulence ', minValue=1.0, value=2.0, parent=ctrlTab)
+            cmds.floatSliderGrp('SizeTurb', field=True, label='Size Turbulence', minValue=0.0, value=0.0, changeCommand=self.updateGeneration, parent=ctrlTab)
+            cmds.floatSliderGrp('ShapeTurb', field=True, label='Shape Turbulence ', minValue=1.0, value=2.0, changeCommand=self.updateGeneration, parent=ctrlTab)
         else:
-            cmds.floatSliderGrp('MinScale', field=True, label='Min Scale ', minValue=0.0, value=1.0, parent=ctrlTab)
-            cmds.floatSliderGrp('MaxScale', field=True, label='Max Scale ', minValue=0.0, value=1.0, parent=ctrlTab)
+            cmds.floatSliderGrp('MinScale', field=True, label='Min Scale ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration, parent=ctrlTab)
+            cmds.floatSliderGrp('MaxScale', field=True, label='Max Scale ', minValue=0.0, value=1.0, changeCommand=self.updateGeneration, parent=ctrlTab)
 
         self.m_landscapeType = opt
 
@@ -79,8 +93,8 @@ class LandscapeGenWnd:
             cmds.warning('No operation history!')
             return
         undoNode = self.m_historyList[numHistory-1]
-        cmds.delete(undoNode)
         self.m_historyList.remove(undoNode)
+        cmds.delete(undoNode)
     
     
     def cancelCallback(self, wndID, *args):
@@ -94,6 +108,22 @@ class LandscapeGenWnd:
 
 
     def applyCallback(self, *args):
+        currentDirty = self.m_isDirty
+        self.m_isDirty = False
+        if not self.m_isRumtimeMode or not currentDirty:
+            self.updateGeneration()
+
+
+    def updateGeneration(self, *args):
+        if self.m_isRumtimeMode and self.m_isDirty:
+            numHistory = len(self.m_historyList)
+            if numHistory != 0:
+                undoNode = self.m_historyList[numHistory-1]
+                self.m_historyList.remove(undoNode)
+                cmds.delete(undoNode)
+        elif not self.m_isRumtimeMode and self.m_isDirty:
+            return
+
         minScale = cmds.floatSliderGrp('MinScale', q=True, v=True)
         maxScale = cmds.floatSliderGrp('MaxScale', q=True, v=True)
         if maxScale < minScale:
@@ -114,6 +144,8 @@ class LandscapeGenWnd:
         else:
             historyNode = lg.generateObj(self.m_landscapeType, density, minScale, maxScale, dirOptIndex, isNaturalGrw, customizedDir=customizedDir)    
             self.m_historyList.append(historyNode)
+
+        self.m_isDirty = True
 
 
 
